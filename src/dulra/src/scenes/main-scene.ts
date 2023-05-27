7c3e3c6b78eb8cfd75b5d3c6d2689fd083857ce8
johnlewissims <johnsimsemail@gmail.com>
2023-05-27 08:34:01 -0600

    Movement Randomized

--- src/dulra/src/scenes/main-scene.ts
+++ src/dulra/src/scenes/main-scene.ts
@@ -3,19 +3,23 @@ import { FabhtConstructor } from '../interfaces/fabht.interface';
 
 export class MainScene extends Phaser.Scene {
   private naFabht: Array<FabhtConstructor>;
+  private naFabhtRead: Array<Fabht>;
+  private timeElapsed: number = 0;
+  private velocityTweenDuration: number = 1000;
 
   constructor(naFabht: Array<FabhtConstructor>) {
     super({ key: 'MainScene' });
     this.naFabht = naFabht;
+    this.naFabhtRead = [];
   }
 
   preload(): void {
-    
+
   }
 
   create(): void {
     for (const [key, fabht] of Object.entries(this.naFabht)) {
-      new Fabht({
+      this.naFabhtRead.push(new Fabht({
         x: fabht.x,
         y: fabht.y,
         width: fabht.width,
@@ -23,7 +27,31 @@ export class MainScene extends Phaser.Scene {
         fillColor: fabht.fillColor,
         xVelocity: fabht.xVelocity,
         yVelocity: fabht.yVelocity
-      }, this,); 
+      }, this,));
     }
   }
+
+  update(time: number, delta: number) {
+    this.timeElapsed += delta;
+
+    // Update velocities every 2 seconds (adjust the interval as desired)
+    if (this.timeElapsed >= 2000) {
+      this.timeElapsed = 0;
+  
+      // Randomize the velocity of square1
+      this.naFabhtRead.forEach(fabht => {
+        const randomVelocity1X = Phaser.Math.Between(-500, 500);
+        const randomVelocity1Y = Phaser.Math.Between(-500, 500)
+        
+        this.tweens.add({
+          targets: fabht.body.velocity,
+          x: randomVelocity1X,
+          y: randomVelocity1Y,
+          duration: this.velocityTweenDuration,
+          ease: 'Linear',
+        });
+      });
+    }
+  }
+
 }
